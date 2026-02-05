f19c478509e02e60770141a9a1bb9a1e048c32c8
Ambarish Roy <ambarishroy1996@gmail.com>
2026-02-05 14:21:25 +0000

    code added

---
+++ shared/TIR.SharedKernel/Events/OcrCompletedEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIR.SharedKernel.Events
+{
+    public sealed record OcrCompletedEvent
+    (
+        Guid TenantId,
+        Guid ProjectId,
+        Guid DocumentId,
+        string OcrTextS3Bucket,
+        string OcrTextS3Key,
+        double AverageConfidence,
+        DateTime ProcessedAtUtc,
+        string CorrelationId
+
+    );
+}
